44f7ae409b5da5b259ddf71c4a0583abcfd94f7d
Martin Randall <mrandall.cont@creditkarma.com>
2019-06-13 16:49:10 +0100

    Tidy up

--- src/main/scala/GridSolver.scala
+++ src/main/scala/GridSolver.scala
@@ -5,19 +5,17 @@ object GridSolver {
     def solveGrid(grid: Grid): Grid =
       if (grid.isFull) grid else grid.potentialMoves() match {
         case head +: _ =>
-          solveGridList(head.possibleValues.map(
+          findSolution(head.possibleValues.map(
             grid.setSquare(head.row, head.col, _)))
         case _ => null
       }
 
-
-    def solveGridList(grids: List[Grid]): Grid =
+    def findSolution(grids: List[Grid]): Grid =
       if (grids.isEmpty) null else solveGrid(grids.head) match {
-        case null => solveGridList(grids.tail)
+        case null => findSolution(grids.tail)
         case solution => solution
       }
 
-
-    solveGridList(List(grid))
+    findSolution(List(grid))
   }
 }
